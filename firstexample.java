6f435d3aab9c9e8e57c3d132de065c3474f90d4d
balakusalavan <balamurugankusalavan@gmail.com>
2026-04-24 14:05:55 +0530

    Update output message in firstexample.java

--- firstexample.java
+++ firstexample.java
@@ -2,7 +2,7 @@ public class firstexample{
 
 public static void main(String[] args){
 
-System.out.println("This is to test jenkins");
+System.out.println("This is to test jenkins files and folders");
 
 }
 
